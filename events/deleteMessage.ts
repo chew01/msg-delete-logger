c7c7ef374f0525d5377802bd3e1bce9e5b2e7115
JunHeng Chew <chewjh19@gmail.com>
2024-10-04 19:07:35 +0800

    download images to mitigate deleted url

--- events/deleteMessage.ts
+++ events/deleteMessage.ts
@@ -1,49 +1,63 @@
+import { unlink } from "node:fs/promises";
 import {
+	AttachmentBuilder,
 	ChannelType,
-	EmbedBuilder,
 	Events,
 	type Message,
 } from "discord.js";
 
-const EXPORT_CHANNEL_ID = process.env.EXPORT_CHANNEL_ID;
+const EXPORT_CHANNEL_ID = process.env.EXPORT_CHANNEL_ID || "";
+const ACCEPTED_TYPES = ["image/png", "image/jpeg", "image/webp"];
+const DOWNLOAD_DIR = "download";
 
 const deleteMessage = {
 	name: Events.MessageDelete,
 	async execute(message: Message) {
-		// Checks for export channel
+		// Check for export channel
 		const exportChannel =
 			await message.client.channels.fetch(EXPORT_CHANNEL_ID);
 		if (!exportChannel || exportChannel.type !== ChannelType.GuildText) {
 			return; // Exit if nowhere to export
 		}
 
-		// Build embed
-		const embed = new EmbedBuilder()
-			.setTitle("New deleted message!")
-			.setAuthor({
-				name: message.author.displayName,
-				iconURL: message.author.avatarURL() || "",
-			})
-			.setDescription(message.content || "No content");
+		// Craft message
+		let msg = `**New deleted message by ${message.author.displayName} with `;
+		msg += `${message.attachments.size} attachment${message.attachments.size === 1 ? "" : "s"}**`;
+		if (message.content) {
+			msg += `\n${message.content}`;
+		}
 
-		const embedList: EmbedBuilder[] = [];
+		const files: AttachmentBuilder[] = [];
+		const filenames: string[] = [];
 
-		// Multiple attachments detected
+		// Attachments detected
 		if (message.attachments.size > 0) {
-			for (const attachment of message.attachments.values()) {
-				if (attachment.contentType?.includes("image")) {
-					const newEmbed = embed.setImage(attachment.url);
-					embedList.push(newEmbed);
+			for (const [id, attachment] of message.attachments) {
+				if (
+					attachment.contentType &&
+					ACCEPTED_TYPES.includes(attachment.contentType)
+				) {
+					try {
+						const res = await fetch(attachment.url);
+						const filename = `${DOWNLOAD_DIR}/${id}_${attachment.name}`;
+						await Bun.write(filename, res);
+
+						files.push(new AttachmentBuilder(filename));
+						filenames.push(filename);
+					} catch (e: unknown) {
+						console.error(e);
+					}
 				}
 			}
 		}
 
-		// No attachments detected
-		if (embedList.length === 0) {
-			embedList.push(embed);
-		}
+		await exportChannel.send({ content: msg, files: files });
 
-		await exportChannel.send({ embeds: [embed] });
+		// Remove files after 10s
+		await Bun.sleep(10000);
+		for (const filename of filenames) {
+			await unlink(filename);
+		}
 	},
 };
 
